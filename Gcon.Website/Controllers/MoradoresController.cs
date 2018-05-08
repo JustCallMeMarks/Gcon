b589ff4e9c03e8b5e80e475b1128eab0ed403c9b
JoaoReva <j.p.b.r@live.com>
2018-05-07 23:45:38 -0300

    Pagina ~/Moradores Apenas sindico

---
+++ Gcon.Website/Controllers/MoradoresController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gcon.Website.Controllers
+{
+    public class MoradoresController : Controller
+    {
+        // GET: Moradores
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
